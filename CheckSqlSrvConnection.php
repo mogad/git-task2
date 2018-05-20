905598efc40f8fdc9fc604a665eaa007834c3650
Mohamed Gad <mohamed.gad@outlook.com>
2018-05-21 01:07:21 +0300

    The version with detailed Server information

--- CheckSqlSrvConnection.php
+++ CheckSqlSrvConnection.php
@@ -4,9 +4,21 @@ $connectionInfo = array( "Database"=>"dbName", "UID"=>"userName", "PWD"=>"passwo
 $conn = sqlsrv_connect( $serverName, $connectionInfo);
 
 if( $conn ) {
-     echo "Connection established.<br />";
+	echo "Connection established.<br />";
+	$server_info = sqlsrv_server_info($conn);
+	echo "\nSql Server Info\n";
+	echo "****************";
+
+	if ($server_info)
+	{
+		foreach($server_info as $key=$value){
+			echo $key . ": " . $value . "\n";		
+		}
+
 }else{
      echo "Connection could not be established.<br />";
      die( print_r( sqlsrv_errors(), true));
 }
+}
+
 ?>
